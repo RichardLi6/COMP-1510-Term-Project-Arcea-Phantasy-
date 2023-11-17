c0390f2ca33bec5807b6bd07f142daff1b3645a3
Richard Maceda <richardmaceda204@gmail.com>
2023-11-16 17:58:34 -0800

    add initial game blueprint

--- game.py
+++ game.py
@@ -3,6 +3,28 @@ Richard Maceda
 A01378156
 Richard Li
 """
+from random import *
+
+
+
+
+def simple_game():
+    print("Hello World, Overwrite this game introduction")
+    chosen_character = choose_character()
+    character = make_character(chosen_character)
+    rows = 10
+    cols = 10
+    board = make_board(rows, cols)
+    describe_current_location(rows, cols, board, character)
+    while True:
+        direction = get_user_choice()
+        # print(direction)
+        valid_move = validate_move(rows, cols, character, direction, board)
+        # print(valid_move)
+        if valid_move:
+            move_character(direction, character, board)
+            describe_current_location(rows, cols, board, character)
+            continue
 
 
 def main():
