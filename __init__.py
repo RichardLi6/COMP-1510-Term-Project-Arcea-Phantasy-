22800a0f2c916083b3eb9ba61458ef310bc814d8
Richard Maceda <richardmaceda204@gmail.com>
2023-11-28 09:34:14 -0800

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+Richard Maceda
+Richard Li
+
+This file is meant to initialize the whole directory as a package.
+By having this file it allows importing modules therefore making the main source code look cleaner
+as the function will have their own separate source code file/ modules.
+"""
+
+
