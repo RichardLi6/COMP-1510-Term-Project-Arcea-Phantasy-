0dbbd261d682acb8eeb8610fc16d79cbb8c53f0a
Richard Maceda <richardmaceda204@gmail.com>
2023-12-03 21:45:46 -0800

    Optimize code for fighting boss

--- game.py
+++ game.py
@@ -105,10 +105,10 @@ def is_alive(character):
 def achieved_goal(character):
 
     level_1_bosses = {
-        1: [{"Name": "North Pillar: Black Tortoise", "Health": 700, "Attack": (10, 20), "Dodge": 0, "Experience": 300},
-        {"Name": "East Pillar: Azure Dragon", "Health": 550, "Attack": (15, 35), "Dodge": 10, "Experience": 300},
-        {"Name": "West Pillar: White Tiger", "Health": 400, "Attack": (15, 35), "Dodge": 20, "Experience": 300},
-        {"Name": "South Pillar: Vermilion Bird", "Health": 500, "Attack": (20, 30), "Dodge": 10, "Experience": 300}]
+        1: {"Name": "North Pillar: Black Tortoise", "Health": 700, "Attack": (10, 20), "Dodge": 0, "Experience": 300},
+        2: {"Name": "East Pillar: Azure Dragon", "Health": 550, "Attack": (15, 35), "Dodge": 10, "Experience": 300},
+        3: {"Name": "West Pillar: White Tiger", "Health": 400, "Attack": (15, 35), "Dodge": 20, "Experience": 300},
+        4: {"Name": "South Pillar: Vermilion Bird", "Health": 500, "Attack": (20, 30), "Dodge": 10, "Experience": 300}
     }
 
     if character["X-coordinate"] == 8 and character["Y-coordinate"] == 1:
