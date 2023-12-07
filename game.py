c2040ac52c544c5e5d173fc9a33769d2c183834e
Richard Maceda <richardmaceda204@gmail.com>
2023-12-06 16:31:29 -0800

    Add spacing in functions to improve readability

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ from random import *
 import character as char
 import random_event
 import battle
-import special_tiles
+import goals
 
 # Non-Built In Module
 import colorama
@@ -17,21 +17,22 @@ from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
 
-#         print(f"{chr(0x2551)} ", end="")
 def describe_current_location(row, col, board, character):
     print(f"\n{Back.BLACK}{chr(0x2554)}{chr(0x2550) * 71}{chr(0x2557)}")
-    for x in range(row):
 
-        # print(f"{Back.BLACK}{Fore.WHITE} --- "*10)
+    for x in range(row):
         print(f"{chr(0x2551)}{Back.BLACK}{Fore.LIGHTWHITE_EX}{"  ---  " * 10} {chr(0x2551)}")
         print(f"{chr(0x2551)}", end="")
-        for y in range(col):
-            if x == character["X-coordinate"] and y == character["Y-coordinate"]:
 
+        for y in range(col):
+
+            if x == character["X-coordinate"] and y == character["Y-coordinate"]:
                 print(f"{Back.BLACK}{Fore.LIGHTWHITE_EX}| {Fore.RESET}{Fore.LIGHTYELLOW_EX} {chr(0x25A0)} {Fore.RESET}"
                       f"{Fore.LIGHTWHITE_EX} |{Fore.RESET}", end="")
+
             else:
                 print(board[(x, y)],end="")
+
         print(f"{Back.BLACK}{Fore.LIGHTWHITE_EX} {chr(0x2551)}")
 
     print(f"{chr(0x2551)}{Back.BLACK}{Fore.WHITE}{"  ---  " * 10} {Fore.LIGHTWHITE_EX}{chr(0x2551)}")
@@ -47,8 +48,10 @@ def make_board(rows, columns):
             # space = f"|{x},{y}|"
             space = "|     |"
             not_space = "|  .  |"
+
             if randint(1, 2) == 2:
                 game_board[(x, y)] = f"{Back.BLACK}{Fore.LIGHTWHITE_EX}{not_space}{Back.RESET}{Fore.RESET}"
+
             else:
                 game_board[(x, y)] = f"{Back.BLACK}{Fore.LIGHTWHITE_EX}{space}{Back.RESET}{Fore.RESET}"
 
@@ -67,7 +70,9 @@ def make_board(rows, columns):
 
 # Prompts the user for Direction
 def get_user_choice():
+
     available_choices = {"1": "north", "2": "east", "3": "south", "4": "west"}
+
     while True:
         user_choice = input(f"\n{chr(0x2554)}{chr(0x2550) * 45}{chr(0x2557)}\n"
                             f"{chr(0x2551)}   Pick a number representing the direction  {chr(0x2551)}\n"
@@ -81,12 +86,14 @@ def get_user_choice():
 
         if user_choice in list(available_choices.keys()):
             return available_choices[user_choice]
+
         else:
             print("Please enter a number that corresponds to a direction")
 
 
 # Validates the move of the user
 def validate_move(rows, columns, character, direction, board):
+
     directions = {"north": -1, "south": 1, "east": 1, "west": -1}
 
     if direction == "north" or direction == "south":
@@ -95,6 +102,7 @@ def validate_move(rows, columns, character, direction, board):
         if to_be_y_coordinate < 0 or to_be_y_coordinate == columns:
             describe_current_location(rows, columns, board, character)
             return False
+
         else:
             return True
 
@@ -104,6 +112,7 @@ def validate_move(rows, columns, character, direction, board):
         if to_be_x_coordinate < 0 or to_be_x_coordinate == rows:
             describe_current_location(rows, columns, board, character)
             return False
+
         else:
             return True
 
@@ -111,15 +120,16 @@ def validate_move(rows, columns, character, direction, board):
 # Will move the character based on the direction
 def move_character(direction, character, board):
     direction_keys = {"north": (0, -1), "east": (1, 0), "south": (0, 1), "west": (-1, 0)}
-
     character["Y-coordinate"] += direction_keys[direction][0]
     character["X-coordinate"] += direction_keys[direction][1]
 
 
 # Checks whether the character is alive
 def is_alive(character):
+
     if character["Health"][0] <= 0:
         return False
+
     else:
         return True
 
@@ -127,24 +137,28 @@ def is_alive(character):
 # Main game
 def simple_game():
     print("\n\n\nHello World, Overwrite this game introduction")
+
     chosen_character = char.choose_character()
     character = char.make_character(chosen_character)
     rows = 10
     cols = 10
     board = make_board(rows, cols)
     describe_current_location(rows, cols, board, character)
+
     while is_alive(character):
         # print(board)
         direction = get_user_choice()
         # print(direction)
         valid_move = validate_move(rows, cols, character, direction, board)
         # print(valid_move)
+
         if valid_move:
             move_character(direction, character, board)
             describe_current_location(rows, cols, board, character)
-            special_tiles.achieved_goal(character, board)
+            goals.achieved_goal(character, board)
             monster = battle.generate_monster()
             you_encountered_a_foe = random_event.random_encounter(monster, character, board)
+
             if you_encountered_a_foe and is_alive(character):
                 battle.fight(character, monster)
                 print("Sheesh")
@@ -152,6 +166,7 @@ def simple_game():
             else:
                 describe_current_location(rows, cols, board, character)
                 print("This is the else block")
+
             char.level_up(character, chosen_character)
 
 
