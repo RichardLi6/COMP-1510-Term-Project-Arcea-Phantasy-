924f88068c653941a0b57a5b32745dc08735c283
Richard Maceda <richardmaceda204@gmail.com>
2023-12-06 17:02:58 -0800

    Prevent User from fighting dead bosses

--- goals.py
+++ goals.py
@@ -7,6 +7,40 @@ A00995183
 import battle
 
 
+def check_if_boss_is_alive(character, boss):
+    if 1 in character["Goal"]:
+        print(f"You already defeated {boss} ")
+        return False
+
+    if 2 in character["Goal"]:
+        print(f"You already defeated {boss} ")
+        return False
+
+    if 3 in character["Goal"]:
+        print(f"You already defeated {boss} ")
+        return False
+
+    if 4 in character["Goal"]:
+        print(f"You already defeated {boss} ")
+        return False
+
+    return True
+
+
+def fight_the_boss(character, boss):
+    battle.fight(character, boss)
+
+    if boss['Health'] <= 0:
+        character["Goals"].append(1)
+        print("You defeated one of the 4 pillars")
+
+    elif boss['Health'] >= 0 and character['Health'][0] > 0:
+        print("You successfully run from the Pillar Boss")
+
+    else:
+        print("You died try, again next time")
+
+
 def in_special_coordinates(character, board):
     if character["X-coordinate"] == 1 and character["Y-coordinate"] == 1 and board["Level"] == 1:
         return True
@@ -36,23 +70,23 @@ def achieved_goal(character, board):
             level_1_bosses = {
                 1: {"Name": "North Pillar: Black Tortoise", "Health": 700, "Attack": (10, 20), "Dodge": 0,
                     "Experience": 300},
-                2: {"Name": "East Pillar: Azure Dragon", "Health": 550, "Attack": (15, 35), "Dodge": 10,
+                2: {"Name": "East Pillar: Azure Dragon", "Health": 550, "Attack": (17, 34), "Dodge": 10,
                     "Experience": 300},
-                3: {"Name": "West Pillar: White Tiger", "Health": 400, "Attack": (15, 35), "Dodge": 20,
+                3: {"Name": "West Pillar: White Tiger", "Health": 400, "Attack": (18, 36), "Dodge": 20,
                     "Experience": 300},
-                4: {"Name": "South Pillar: Vermilion Bird", "Health": 500, "Attack": (20, 30), "Dodge": 10,
-                    "Experience": 300}
+                4: {"Name": "South Pillar: Vermilion Bird", "Health": 500, "Attack": (20, 42), "Dodge": 10,
+                    "Experience": 350}
             }
             print("You have reached one of the four pillars of this stage")
-            boss = which_boss(character)
-            battle.fight(character, level_1_bosses[boss])
 
-            if level_1_bosses[1]['Health'] <= 0:
-                print("You defeated one of the 4 pillars")
-            elif level_1_bosses[1]['Health'] >= 0 and character['Health'][0] > 0:
-                print("You successfully run from the Pillar Boss")
+            boss = which_boss(character)
+
+            if not check_if_boss_is_alive(character, level_1_bosses[boss]):
+                return
+
             else:
-                print("You died try, again next time")
+                fight_the_boss(character, level_1_bosses[boss])
+
     else:
         return
 
