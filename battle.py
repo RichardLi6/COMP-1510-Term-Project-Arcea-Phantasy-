61523dabd5a07b60d59816e0d38578b487c28bd7
Richard Maceda <richardmaceda204@gmail.com>
2023-12-08 01:37:27 -0800

    Add Doc Strings for functions

--- battle.py
+++ battle.py
@@ -15,6 +15,16 @@ colorama.init(autoreset=True)
 
 # Heal option for User
 def heal_character(character):
+    """
+    function regenerates characters health and mana
+
+    :param character: a dictionary of the character
+    :pre-condition: character must be a dictionary containing the user's character values
+    :pre-condition: character Mana key has value greater than 0
+    :pre-condition: character Health key has value greater than 0
+    :post-condition: regenerate the user mana base on mana scaling
+    :post-condition: regenerate the user health base on health scaling
+    """
     heal_scaling = {1: 11, 2: 15, 3: 17, 4: 20, 5: 23, 6: 28}
     mana_scaling = {1: 13, 2: 17, 3: 21, 4: 25, 5: 28, 6: 35}
 
@@ -31,6 +41,12 @@ def heal_character(character):
 
 # Function that generates a monster
 def generate_monster():
+    """
+    function generates a monster
+
+    :post-condition: generate a random monster during a random encounter
+    :return: return a dictionary containing the monster's stats
+    """
     beginner_monsters_list = {
         1: {"Name": "Slime", "Health": 60, "Attack": (7, 10), "Experience": 15},
         2: {"Name": "Undead", "Health": 80, "Attack": (5, 7), "Experience": 15},
@@ -44,6 +60,16 @@ def generate_monster():
 
 # Prints the introduction whenever fighting
 def fight_introduction(character, monster):
+    """
+    function prints battle stats of character and monster
+
+    :param character: a dictionary of the character
+    :para monster: a dictionary of the monster
+    :precondition: character must be a dictionary containing the user's character values
+    :precondition: monster must be a dictionary containing the monster's stats
+    :post-condition: prints the users health and mana
+    :post-condition: prints the monster's health
+    """
     print()
     print(chr(0x2550) * 30)
     print("You:")
@@ -59,6 +85,16 @@ def fight_introduction(character, monster):
 
 # A Text confirmation after user wins a fight
 def after_fight(character, monster):
+    """
+    function prints the after effects winning a fight
+
+    :param character: a dictionary of the character
+    :para monster: a dictionary of the monster
+    :precondition: character must be a dictionary containing the user's character values
+    :precondition: monster must be a dictionary containing the monster's stats
+    :post-condition: inform the user winning against the monster and its rewards
+    :post-condition: regenerate both mana and health  of the character
+    """
     print()
     print((chr(0x2550) * 15) + " You defeated the monster " + (chr(0x2550) * 15))
     print()
@@ -82,6 +118,13 @@ def after_fight(character, monster):
 
 # Passive Regeneration of Character during fights
 def passive_regeneration(character):
+    """
+    function regenerates characters health and mana slightly
+
+    :param character: a dictionary of the character
+    :precondition: character must be a dictionary containing the user's character values
+    :post-condition: regenerates the character's health and mana during fights
+    """
     character["Mana"][0] = min(character["Mana"][0] + 5, character["Mana"][1])
     print("Your mana passively replenish by 5")
 
@@ -91,6 +134,15 @@ def passive_regeneration(character):
 
 # Print How much you damage the monster
 def normal_attack_description(character, monster):
+    """
+    function prints description of character normal attack damage
+
+    :param character: a dictionary of the character
+    :para monster: a dictionary of the monster
+    :precondition: character must be a dictionary containing the user's character values
+    :precondition: monster must be a dictionary containing the monster's stats
+    :post-condition: prints how much the user's character damage the monster
+    """
     print(f"You slashed the monster for {Fore.WHITE}{character['Attack']}{Fore.RESET} leaving its Health "
           f"{Fore.LIGHTRED_EX}{monster['Health']}")
     print()
@@ -98,6 +150,12 @@ def normal_attack_description(character, monster):
 
 # Prompts user for choice when in fight
 def user_prompt():
+    """
+    function prompts user action when in fight
+
+    :post-condition: ask user what they want to do whenever they encounter a monster
+    :return: a number in string format corresponding to user's choice
+    """
     user_input = input(f"\n{chr(0x2554)}{chr(0x2550) * 27}{chr(0x2557)}\n"
                        f"{chr(0x2551)}  What do you want to do:  {chr(0x2551)}\n"
                        f"{chr(0x2551)}                           {chr(0x2551)}\n"
@@ -112,6 +170,16 @@ def user_prompt():
 
 # Function whenever Character is in a fight
 def fight(character, monster):
+    """
+    function runs the fight of character and monster
+
+    :param character: a dictionary of the character
+    :para monster: a dictionary of the monster
+    :precondition: character must be a dictionary containing the user's character values
+    :precondition: monster must be a dictionary containing the monster's stats
+    :post-condition: runs the sequence of command involve during the character fighting the monster
+    :post-condition: function ends when character dies or monster dies
+    """
     user_choices = ("1", "2", "3", "4")
 
     while character["Health"][0] >= 0 and monster["Health"] > 0:
@@ -161,6 +229,17 @@ def fight(character, monster):
 
 # Monsters Turn
 def monster_attack(character, monster):
+    """
+    function runs the monster's turn again character in fights
+
+    :param character: a dictionary of the character
+    :para monster: a dictionary of the monster
+    :precondition: character must be a dictionary containing the user's character values
+    :precondition: monster must be a dictionary containing the monster's stats
+    :post-condition: subtracts the character's health base in monster attack
+    :post-condition: if a critical hit occurs subtracts the character's health base on monster's critical attack
+    """
+
     critical = randint(1, 10)
     critical_range = [1, 3, 7]
     if critical not in critical_range:
@@ -172,11 +251,16 @@ def monster_attack(character, monster):
         print(f"The monster attack you for {monster['Attack'][1]} leaving you with {character['Health'][0]} health \n")
         character["Health"][0] -= monster["Attack"][1]
 
-    return
-
 
 # Prints Users Usable Skills
 def print_user_skills(user):
+    """
+    function prints user's available to use skills
+
+    :param user: a dictionary of the character
+    :precondition: user must be a dictionary containing the user's character values
+    :post-condition: prints the user's character available skills for use
+    """
     index = 1
     max_skill_name_length = max(len(skill[0]) for skill in user["Skill"].values())
     print(chr(0x2550) * 60)
@@ -198,6 +282,17 @@ def print_user_skills(user):
 
 
 def fight_with_skill(character, monster):
+    """
+    function runs the commands when character pick skill attack option from user prompt
+
+    :param character: a dictionary of the character
+    :para monster: a dictionary of the monster
+    :precondition: character must be a dictionary containing the user's character values
+    :precondition: monster must be a dictionary containing the monster's stats
+    :post-condition: Character will use consume mana base on the skill being use
+    :post-condition: Monster will take damage base on the attack of the skill being used
+    :return: None
+    """
     print_user_skills(character)
 
     while True:
@@ -231,6 +326,14 @@ def fight_with_skill(character, monster):
 
 
 def try_to_flee_successfully(character):
+    """
+    function when user is trying to flee in the final boss stage
+
+    :param character: a dictionary of the character
+    :precondition: character must be a dictionary containing the user's character values
+    :post-condition: character will be able to run from the final boss at 70% chance
+    :return: return Boolean True if character escaped successfully otherwise Boolean False
+    """
     chance = random.randint(1, 10)
     cannot_flee_chance = (1, 5, 10)
 
