2779ee348a0605f4552a3ba0e75299dfefea4190
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 14:30:49 -0800

    Initial Commit

---
+++ test_make_character.py
@@ -0,0 +1,11 @@
+"""
+Richard Maceda
+A01378156
+"""
+from unittest import TestCase
+from character import make_character
+
+
+class TestMakeCharacter(TestCase):
+    def test_make_character(self):
+        self.fail()
