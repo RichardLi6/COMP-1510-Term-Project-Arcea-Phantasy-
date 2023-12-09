ce46bcdac271d74a64cada49efb6cd5953948487
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 14:20:44 -0800

    Initial Commit

---
+++ test_move_character.py
@@ -0,0 +1,18 @@
+"""
+Richard Maceda
+A01378156
+"""
+from unittest import TestCase
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+class TestMoveCharacter(TestCase):
+    def test_move_character(self):
+        self.fail()
