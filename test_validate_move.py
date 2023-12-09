b3f113827d5918d23c348301411b8eb8f53e2dcd
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 13:52:38 -0800

    Add 2 Test Cases

--- test_validate_move.py
+++ test_validate_move.py
@@ -4,12 +4,39 @@ A01378156
 Richard Li
 A00995183
 """
-
-import io
-from unittest.mock import patch
+from board import validate_move
 from unittest import TestCase
 
 
 class TestValidateMove(TestCase):
-    def test_validate_move(self):
-        self.fail()
+    def test_valid_move(self):
+        board_1 = {
+            (0, 0): "| |", (1, 0): "| |", (2, 0): "| |",
+            (0, 1): "| |", (1, 1): "| |", (2, 1): "| |",
+            (0, 2): "| |", (1, 2): "| |", (2, 2): "| |"
+        }
+        row = 3
+        col = 3
+        character = {'X-coordinate': 1, 'Y-coordinate': 1}
+        direction = "west"
+        expected_output = True
+        actual_output = validate_move(row, col, character, direction, board_1)
+        self.assertEqual(expected_output, actual_output)
+
+    def test_invalid_move(self):
+        board_2 = {
+            (0, 0): "| |", (1, 0): "| |", (2, 0): "| |",
+            (0, 1): "| |", (1, 1): "| |", (2, 1): "| |",
+            (0, 2): "| |", (1, 2): "| |", (2, 2): "| |"
+        }
+        row = 3
+        col = 3
+        character = {'X-coordinate': 0, 'Y-coordinate': 0}
+        direction = "west"
+        expected_output = False
+        actual_output = validate_move(row, col, character, direction, board_2)
+        self.assertEqual(expected_output, actual_output)
+
+
+
+
