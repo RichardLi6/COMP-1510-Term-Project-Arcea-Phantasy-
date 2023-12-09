ef3ba35b58102e84c4f8a0ecdb5f03e532a8d123
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 15:25:56 -0800

    Add 2 Test Case

--- test_battle_is_alive.py
+++ test_battle_is_alive.py
@@ -3,8 +3,18 @@ Richard Maceda
 A01378156
 """
 from unittest import TestCase
+from battle import is_alive
 
 
 class TestIsAlive(TestCase):
-    def test_is_alive(self):
-        self.fail()
+    def test_character_is_alive(self):
+        character = {"Health": [10, 40]}
+        actual = is_alive(character)
+        expected = True
+        self.assertEqual(expected, actual)
+
+    def test_character_is_not_alive(self):
+        character = {"Health": [0, 40]}
+        actual = is_alive(character)
+        expected = False
+        self.assertEqual(expected, actual)
