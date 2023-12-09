3892eaf3714e1722439f620c22525287788c0325
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 15:48:39 -0800

    Initial Commit

---
+++ test_battle_generate_monster.py
@@ -0,0 +1,14 @@
+"""
+Richard Maceda
+A01378156
+"""
+
+import io
+from unittest.mock import patch
+from unittest import TestCase
+from simple_game import guessing_game
+
+
+class TestGenerateMonster(TestCase):
+    def test_generate_monster(self):
+        self.fail()
