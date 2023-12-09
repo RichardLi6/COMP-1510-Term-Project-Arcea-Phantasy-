6a7334981bf75fbc49b9839b3e7eca975aeb8349
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 14:43:27 -0800

    Initial Commit

---
+++ test_character_new_skills.py
@@ -0,0 +1,13 @@
+"""
+Richard Maceda
+A01378156
+Richard Li
+A00995183
+"""
+from unittest import TestCase
+from character import new_skills
+
+
+class TestNewSkills(TestCase):
+    def test_new_skills(self):
+        self.fail()
