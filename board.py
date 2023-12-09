9be3e19f31cec47fc54585ac84ae0fe9169bb4d3
Richard Maceda <richardmaceda204@gmail.com>
2023-12-08 18:45:39 -0800

    Modularize 3 functions to board module

--- board.py
+++ board.py
@@ -93,6 +93,88 @@ def make_board(rows, columns):
     return game_board
 
 
+# Prompts the user for Direction
+def get_user_choice():
+    """
+    function prompts the user what to do when not in fight
+
+    :precondition: user will enter an infinite loop until entering a valid input
+    :post-condition: prompt user for a direction to go to
+    """
+    available_choices = {"1": "north", "2": "east", "3": "west", "4": "south"}
+
+    while True:
+        user_choice = input(f"\n{chr(0x2554)}{chr(0x2550) * 45}{chr(0x2557)}\n"
+                            f"{chr(0x2551)}   Pick a number representing the direction  {chr(0x2551)}\n"
+                            f"{chr(0x2551)}{' ' * 45}{chr(0x2551)}\n"
+                            f"{chr(0x2551)}{Fore.LIGHTBLUE_EX}    1 = North  {' ' * 30}{Fore.RESET}{chr(0x2551)}\n"
+                            f"{chr(0x2551)}{Fore.LIGHTYELLOW_EX}    2 = East   {' ' * 30}{Fore.RESET}{chr(0x2551)}\n"
+                            f"{chr(0x2551)}{Fore.LIGHTCYAN_EX}    3 = West   {' ' * 30}{Fore.RESET}{chr(0x2551)}\n"
+                            f"{chr(0x2551)}{Fore.LIGHTMAGENTA_EX}    4 = South  {' ' * 30}{Fore.RESET}{chr(0x2551)}\n"
+                            f"{chr(0x255A)}{chr(0x2550) * 45}{chr(0x255D)}\n"
+                            "Type number of corresponding action: \n").strip()
+
+        if user_choice in list(available_choices.keys()):
+            return available_choices[user_choice]
+
+        else:
+            print("Please enter a number that corresponds to a direction")
+
+
+# Validates the move of the user
+def validate_move(rows, columns, character, direction, board):
+    """
+    function validates the move of user
+
+    :param rows: an integer representing the rows in the game
+    :param columns: an integer representing the col in game
+    :param board: a dictionary representing the coordinate values
+    :param character: a dictionary representing the user character stats
+    :param direction: a string representing a direction
+    :post-condition: validate if the direction pass is valid
+    :return: return True if direction is valid
+    :return: return False if direction is invalid
+    """
+    directions = {"north": -1, "south": 1, "east": 1, "west": -1}
+
+    if direction == "north" or direction == "south":
+        to_be_y_coordinate = character["X-coordinate"] + directions[direction]
+
+        if to_be_y_coordinate < 0 or to_be_y_coordinate == rows:
+            board.describe_current_location(rows, columns, board, character)
+            print("You cannot go there")
+            return False
+
+        else:
+            return True
+
+    if direction == "west" or direction == "east":
+        to_be_x_coordinate = character["Y-coordinate"] + directions[direction]
+
+        if to_be_x_coordinate < 0 or to_be_x_coordinate == columns:
+            board.describe_current_location(rows, columns, board, character)
+            print("You cannot go there")
+            return False
+
+        else:
+            return True
+
+
+# Will move the character based on the direction
+def move_character(direction, character):
+    """
+    function moves the character in board
+
+    :param direction: a string representing a direction
+    :param character: a dictionary representing the user character's stats
+    :pre-condition: direction must already be validated
+    :post-condition: modify character's X_coordinate value and Y_coordinate corresponding to direction
+    """
+    direction_keys = {"north": (0, -1), "east": (1, 0), "south": (0, 1), "west": (-1, 0)}
+    character["Y-coordinate"] += direction_keys[direction][0]
+    character["X-coordinate"] += direction_keys[direction][1]
+
+
 def main():
     pass
 
