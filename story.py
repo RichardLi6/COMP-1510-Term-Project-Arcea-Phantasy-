0f5034aad0e6adcfcfac6009507b1bb1f3c5015b
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 11:05:58 -0800

    Update Story

--- story.py
+++ story.py
@@ -15,7 +15,7 @@ def print_game_introduction():
     print("The winds of destiny carry you to the brink of perilous adventure. \nAs the chosen hero, your quest begins"
           "in a world shrouded in mystery and threatened by an ancient darkness.\nFour powerful mini bosses guard the"
           " path to salvation,\neach challenging your strength and cunning Uncover the secrets of the Realms,\n"
-          f"{Fore.LIGHTYELLOW_EX}gather the 4 essence needed to confront the Final Boss, "
+          f"{Fore.LIGHTYELLOW_EX}gather the 4 essence, "
           f"and shape the fate of the world.")
 
 
