c7c6e8953cb258e0f18c3875308c85261d49b845
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 13:23:16 -0800

    Initial Commit

---
+++ test_validate_move.py
@@ -0,0 +1,15 @@
+"""
+Richard Maceda
+A01378156
+Richard Li
+A00995183
+"""
+
+import io
+from unittest.mock import patch
+from unittest import TestCase
+
+
+class TestValidateMove(TestCase):
+    def test_validate_move(self):
+        self.fail()
