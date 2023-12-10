1d54aac31d4d1106dcd10e30bcdbe49e27401e49
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 17:03:19 -0800

    Initial Commit

---
+++ test_battle_try_to_flee_successfully.py
@@ -0,0 +1,13 @@
+"""
+Richard Maceda
+A01378156
+Richard Li
+A00995183
+"""
+from unittest import TestCase
+from battle import try_to_flee_successfully
+
+
+class TestTryToFleeSuccessfully(TestCase):
+    def test_try_to_flee_successfully(self):
+        self.fail()
