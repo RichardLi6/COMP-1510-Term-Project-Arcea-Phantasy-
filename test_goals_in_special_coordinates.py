ae8f666dd563b2b781390304c6563c9a65bbb43c
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 16:36:52 -0800

    Initial Commit

---
+++ test_goals_in_special_coordinates.py
@@ -0,0 +1,13 @@
+"""
+Richard Maceda
+A01378156
+Richard Li
+A00995183
+"""
+from unittest import TestCase
+from goals import in_special_coordinates
+
+
+class TestInSpecialCoordinates(TestCase):
+    def test_in_special_coordinates(self):
+        self.fail()
