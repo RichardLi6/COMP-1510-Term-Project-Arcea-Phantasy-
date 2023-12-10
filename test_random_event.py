688f7cfb906c88f10acd0381265e3f2524bf00b3
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 17:22:54 -0800

    Initial Commit

---
+++ test_random_event.py
@@ -0,0 +1,14 @@
+"""
+Richard Maceda
+A01378156
+Richard Li
+A00995183
+"""
+from unittest.mock import patch
+from unittest import TestCase
+from random_event import random_encounter
+
+
+class Test(TestCase):
+    def test_random_encounter(self):
+        self.fail()
