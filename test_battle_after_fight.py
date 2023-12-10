315c89cdbdf152c2febd95fd77f88d4cc11318a2
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 17:15:30 -0800

    Add a Test Case

--- test_battle_after_fight.py
+++ test_battle_after_fight.py
@@ -10,4 +10,8 @@ from battle import after_fight
 
 class TestAfterFight(TestCase):
     def test_after_fight(self):
-        self.fail()
+        monster = {"Name": "Cthulhu", "Health": 50, "Attack": (10, 10), "Experience": 15}
+        actual_character = {"Health": [10, 50], "Mana": [10, 80], "Experience": 0}
+        after_fight(actual_character, monster)
+        expected_character = {"Health": [35, 50], "Mana": [45, 80], "Experience": 15}
+        self.assertEqual(expected_character, actual_character)
