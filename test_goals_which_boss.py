0e2245bde603e461fdeb47a0e623d7150bcdbdf9
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 16:34:23 -0800

    Add 4 Test Case

--- test_goals_which_boss.py
+++ test_goals_which_boss.py
@@ -10,5 +10,26 @@ from unittest.mock import patch
 
 
 class TestWhichBoss(TestCase):
-    def test_which_boss(self):
-        self.fail()
+    def test_boss_1(self):
+        character = {"X-coordinate": 1, "Y-coordinate": 1}
+        actual = which_boss(character)
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    def test_boss_2(self):
+        character = {"X-coordinate": 1, "Y-coordinate": 23}
+        actual = which_boss(character)
+        expected = 2
+        self.assertEqual(expected, actual)
+
+    def test_boss_3(self):
+        character = {"X-coordinate": 8, "Y-coordinate": 1}
+        actual = which_boss(character)
+        expected = 3
+        self.assertEqual(expected, actual)
+
+    def test_boss_4(self):
+        character = {"X-coordinate": 8, "Y-coordinate": 23}
+        actual = which_boss(character)
+        expected = 4
+        self.assertEqual(expected, actual)
