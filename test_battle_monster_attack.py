d64efc9cc5fb24ba46c0ebeb844e89ddbc2de661
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 16:15:13 -0800

    Initial Commit

---
+++ test_battle_monster_attack.py
@@ -0,0 +1,11 @@
+"""
+Richard Maceda
+A01378156
+"""
+from unittest import TestCase
+from battle import monster_attack
+
+
+class TestMonsterAttack(TestCase):
+    def test_monster_attack(self):
+        self.fail()
