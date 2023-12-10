9fdf2062efa5fd716d2cc05ba53e2980bd7b2ee2
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 16:07:59 -0800

    Initial Commit

---
+++ test_battle_passive_regeneration.py
@@ -0,0 +1,11 @@
+"""
+Richard
+Maceda
+"""
+from unittest import TestCase
+from battle import passive_regeneration
+
+
+class TestPassiveRegeneration(TestCase):
+    def test_passive_regeneration(self):
+        self.fail()
