60e2ded98d1c42d05c456105b65bfe1223c84887
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 16:59:36 -0800

    Add 4 Test Case

--- test_goals_check_if_win.py
+++ test_goals_check_if_win.py
@@ -9,5 +9,22 @@ from goals import check_if_win
 
 
 class TestCheckIfWin(TestCase):
-    def test_check_if_win(self):
-        self.fail()
+    def test_user_win_while_fighting_all_4_boss_in_order(self):
+        character = {"Goal": [1, 2, 3, 4], "Final Boss": True}
+        actual = check_if_win(character)
+        expected = True
+
+    def test_user_win_while_fighting_all_4_boss_but_not_in_order(self):
+        character = {"Goal": [1, 2, 3, 4], "Final Boss": True}
+        actual = check_if_win(character)
+        expected = True
+
+    def test_user_only_killed_3_boss(self):
+        character = {"Goal": [1, 2, 3], "Final Boss": False}
+        actual = check_if_win(character)
+        expected = False
+
+    def test_user_did_not_kill_any_boss(self):
+        character = {"Goal": [1, 2, 3], "Final Boss": False}
+        actual = check_if_win(character)
+        expected = False
