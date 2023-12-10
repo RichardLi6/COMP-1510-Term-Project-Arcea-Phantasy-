85b21092054a15d0429542bda64f2c0fd0c5b7f7
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 16:12:23 -0800

    Add Test Case

--- test_battle_passive_regeneration.py
+++ test_battle_passive_regeneration.py
@@ -1,6 +1,6 @@
 """
-Richard
-Maceda
+Richard Maceda
+A01378156
 """
 from unittest import TestCase
 from battle import passive_regeneration
@@ -8,4 +8,7 @@ from battle import passive_regeneration
 
 class TestPassiveRegeneration(TestCase):
     def test_passive_regeneration(self):
-        self.fail()
+        actual_character = {"Health": [10, 50], "Mana": [10, 80]}
+        passive_regeneration(actual_character)
+        expected_character = {"Health": [14, 50], "Mana": [15, 80]}
+        self.assertEqual(expected_character, actual_character)
