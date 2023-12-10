dfa619d4e8705fa997714f2588d1ba26d974d0d5
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 16:41:18 -0800

    Add 5 Test Case

--- test_goals_in_special_coordinates.py
+++ test_goals_in_special_coordinates.py
@@ -9,5 +9,38 @@ from goals import in_special_coordinates
 
 
 class TestInSpecialCoordinates(TestCase):
-    def test_in_special_coordinates(self):
-        self.fail()
+
+    def test_not_special(self):
+        board = {"Level": 1}
+        character = {"X-coordinate": 4, "Y-coordinate": 4}
+        actual = in_special_coordinates(character, board)
+        expected = False
+        self.assertEqual(expected, actual)
+
+    def test_boss_1(self):
+        board = {"Level": 1}
+        character = {"X-coordinate": 1, "Y-coordinate": 1}
+        actual = in_special_coordinates(character, board)
+        expected = True
+        self.assertEqual(expected, actual)
+
+    def test_boss_2(self):
+        board = {"Level": 1}
+        character = {"X-coordinate": 8, "Y-coordinate": 23}
+        actual = in_special_coordinates(character, board)
+        expected = True
+        self.assertEqual(expected, actual)
+
+    def test_boss_3(self):
+        board = {"Level": 1}
+        character = {"X-coordinate": 8, "Y-coordinate": 1}
+        actual = in_special_coordinates(character, board)
+        expected = True
+        self.assertEqual(expected, actual)
+
+    def test_boss_4(self):
+        board = {"Level": 1}
+        character = {"X-coordinate": 8, "Y-coordinate": 23}
+        actual = in_special_coordinates(character, board)
+        expected = True
+        self.assertEqual(expected, actual)
