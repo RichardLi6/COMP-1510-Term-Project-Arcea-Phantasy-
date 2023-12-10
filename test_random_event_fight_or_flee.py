95fec272c99aa16deae34bc76df3ffe084a5023e
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 17:36:58 -0800

    Add 2 Test Cases

--- test_random_event_fight_or_flee.py
+++ test_random_event_fight_or_flee.py
@@ -4,9 +4,22 @@ A01378156
 Richard Li
 A00995183
 """
+from unittest.mock import patch
 from unittest import TestCase
 from random_event import fight_or_flee
 
+
 class TestFightOrFlee(TestCase):
-    def test_fight_or_flee(self):
-        self.fail()
+    @patch('builtins.input', side_effect=['1'])
+    def test_fight(self, _):
+        monster = {"Name": "Fallen", "Health": 20, "Attack": (12, 15), "Experience": 10}
+        actual = fight_or_flee(monster)
+        expected = True
+        self.assertEqual(expected, actual)
+
+    @patch('builtins.input', side_effect=['2'])
+    def test_flee(self, _):
+        monster = {"Name": "Fallen", "Health": 20, "Attack": (12, 15), "Experience": 10}
+        actual = fight_or_flee(monster)
+        expected = False
+        self.assertEqual(expected, actual)
