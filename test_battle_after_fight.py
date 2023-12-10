ec7a4db6da52f554d41f0b059c5214bbe50693e7
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 17:11:07 -0800

    Initial Commit

---
+++ test_battle_after_fight.py
@@ -0,0 +1,13 @@
+"""
+Richard Maceda
+A01378156
+Richard Li
+A00995183
+"""
+from unittest import TestCase
+from battle import after_fight
+
+
+class TestAfterFight(TestCase):
+    def test_after_fight(self):
+        self.fail()
