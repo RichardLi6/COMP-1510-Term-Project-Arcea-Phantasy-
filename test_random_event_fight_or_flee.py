b70be0c7a1c06d0985db39d69aa049614ccaa60b
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 17:33:23 -0800

    Initial Commit

---
+++ test_random_event_fight_or_flee.py
@@ -0,0 +1,12 @@
+"""
+Richard Maceda
+A01378156
+Richard Li
+A00995183
+"""
+from unittest import TestCase
+from random_event import fight_or_flee
+
+class TestFightOrFlee(TestCase):
+    def test_fight_or_flee(self):
+        self.fail()
