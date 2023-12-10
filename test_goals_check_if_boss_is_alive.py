5d81b4e271068dc47e1be453972f2b26475f7586
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 16:43:39 -0800

    Initial Commit

---
+++ test_goals_check_if_boss_is_alive.py
@@ -0,0 +1,13 @@
+"""
+Richard Maceda
+A01378156
+Richard Li
+A00995183
+"""
+from unittest import TestCase
+from goals import check_if_boss_is_alive
+
+
+class TestCheckIfBossIsAlive(TestCase):
+    def test_check_if_boss_is_alive(self):
+        self.fail()
