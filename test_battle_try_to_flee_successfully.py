45d6d9c5ebc78c5285d9f89d8cc256dd14d65fa0
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 17:09:24 -0800

    Add 2 Test Case

--- test_battle_try_to_flee_successfully.py
+++ test_battle_try_to_flee_successfully.py
@@ -5,9 +5,21 @@ Richard Li
 A00995183
 """
 from unittest import TestCase
+from unittest.mock import patch
 from battle import try_to_flee_successfully
 
 
 class TestTryToFleeSuccessfully(TestCase):
-    def test_try_to_flee_successfully(self):
-        self.fail()
+    @patch('battle.randint', return_value=1)
+    def test_not_flee_successfully(self, _):
+        character = {"Health": [50, 50]}
+        actual = try_to_flee_successfully(character)
+        expected = True
+        self.assertEqual(expected, actual)
+
+    @patch('battle.randint', return_value=2)
+    def test_flee_successfully(self, _):
+        character = {"Health": [50, 50]}
+        actual = try_to_flee_successfully(character)
+        expected = False
+        self.assertEqual(expected, actual)
