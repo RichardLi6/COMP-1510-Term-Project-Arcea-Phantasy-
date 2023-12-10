9fbbe6016ee69ab1ee4077bc848f2227f36d0bf9
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 16:55:25 -0800

    Initial Commit

---
+++ test_goals_check_if_win.py
@@ -0,0 +1,13 @@
+"""
+Richard Maceda
+A01378156
+Richard Li
+A00995183
+"""
+from unittest import TestCase
+from goals import check_if_win
+
+
+class TestCheckIfWin(TestCase):
+    def test_check_if_win(self):
+        self.fail()
