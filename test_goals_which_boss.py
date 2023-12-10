edbd44c59fc277f56d56f86c10d0da2de6bdad99
Richard Maceda <richardmaceda204@gmail.com>
2023-12-09 16:26:45 -0800

    Initial Commit

---
+++ test_goals_which_boss.py
@@ -0,0 +1,14 @@
+"""
+Richard Maceda
+A01378156
+Richard Li
+A00995183
+"""
+from unittest import TestCase
+from goals import which_boss
+from unittest.mock import patch
+
+
+class TestWhichBoss(TestCase):
+    def test_which_boss(self):
+        self.fail()
